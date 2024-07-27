ed2a408335c82d705d877c994d8da277ee3020b0
huytran2511 <huyqtran96@gmail.com>
2024-07-27 14:50:47 -0700

    added function so read in csv for genres and convert subgenres to genres

--- top3-genres-liked-songs.py
+++ top3-genres-liked-songs.py
@@ -1,11 +1,22 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from collections import Counter
+import csv
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="cea88769eddc49e0b7f7ed76ee197517",
                                                client_secret="a7c55ba17fc94e7584175a25c93c2174",
                                                redirect_uri="http://localhost:8080",
                                                scope="user-library-read"))
+
+# Load the CSV file and create a mapping dictionary
+genres_dict = {}
+csv_file_path = 'subgenres.csv'
+
+with open(csv_file_path, mode='r', encoding='utf-8-sig') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        genre, subgenre = row
+        genres_dict[subgenre] = genre
 
 def get_all_saved_tracks(sp):
     results = sp.current_user_saved_tracks()
@@ -27,25 +38,35 @@ def get_genres_from_tracks(sp, tracks):
     
     return genres
 
-def get_top_genres(genres, top_n):
-    genre_counts = Counter(genres)
+def convert_to_genre(subgenres, mapping):
+    genres = [mapping.get(subgenre, "unknown") for subgenre in subgenres]
+    
+    return genres
+
+def get_top_genres(subgenres, top_n):
+    genres = convert_to_genre(subgenres, genres_dict)
+    # print()
+    # print(genres)
+    filtered_genres = [genre for genre in genres if genre != "unknown"]
+    genre_counts = Counter(filtered_genres)
     top_genres = genre_counts.most_common(top_n)
     top_genre_names = [genre for genre, count in top_genres]
-    
+
     return top_genre_names
 
 # Fetch all liked tracks
-all_tracks = get_all_saved_tracks(sp)
+# all_tracks = get_all_saved_tracks(sp)
 
-# results = sp.current_user_saved_tracks()
-# all_tracks = results['items']
+results = sp.current_user_saved_tracks()
+all_tracks = results['items']
 
 # Extract genres from those tracks
-genres = get_genres_from_tracks(sp, all_tracks)
+subgenres = get_genres_from_tracks(sp, all_tracks)
+# print(subgenres)
 
-# # Determine the top 3 genres
-top_genres = get_top_genres(genres, top_n=3)
+# Determine the top 3 genres
+top3_genres = get_top_genres(subgenres, top_n=3)
 
 # Print the top 3 genres
 print("\nTop 3 Genres:")
-print(top_genres)
+print(top3_genres)
--- top3-genres-top-artists.py
+++ top3-genres-top-artists.py
@@ -1,6 +1,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from collections import Counter
+import csv
 
 # Initialize Spotipy with user authorization
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="cea88769eddc49e0b7f7ed76ee197517",
@@ -8,13 +9,23 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="cea88769eddc49e0b7f7ed
                                                redirect_uri="http://localhost:8080",
                                                scope="user-top-read"))
 
+# Load the CSV file and create a mapping dictionary
+genres_dict = {}
+csv_file_path = 'subgenres.csv'
+
+with open(csv_file_path, mode='r', encoding='utf-8-sig') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        genre, subgenre = row
+        genres_dict[subgenre] = genre
+
 def get_top_artists(sp, limit):
     results = sp.current_user_top_artists(limit=limit)
     artists = results['items']
     
-    # while results['next']:
-    #     results = sp.next(results)
-    #     artists.extend(results['items'])
+    while results['next']:
+        results = sp.next(results)
+        artists.extend(results['items'])
     
     return artists
 
@@ -25,8 +36,15 @@ def get_genres_from_artists(sp, artists):
     
     return genres
 
-def get_top_genres(genres, top_n):
-    genre_counts = Counter(genres)
+def convert_to_genre(subgenres, mapping):
+    genres = [mapping.get(subgenre, "unknown") for subgenre in subgenres]
+    
+    return genres
+
+def get_top_genres(subgenres, top_n):
+    genres = convert_to_genre(subgenres, genres_dict)
+    filtered_genres = [genre for genre in genres if genre != "unknown"]
+    genre_counts = Counter(filtered_genres)
     top_genres = genre_counts.most_common(top_n)
     top_genre_names = [genre for genre, count in top_genres]
     
@@ -36,11 +54,12 @@ def get_top_genres(genres, top_n):
 top_artists = get_top_artists(sp, limit=50)
 
 # Extract genres from those artists
-genres = get_genres_from_artists(sp, top_artists)
+subgenres = get_genres_from_artists(sp, top_artists)
+print(subgenres)
 
 # Determine the top 3 genres
-top_genres = get_top_genres(genres, top_n=3)
+top3_genres = get_top_genres(subgenres, top_n=3)
 
 # Print the top 3 genres
 print("\nTop 3 Genres:")
-print(top_genres)
+print(top3_genres)
